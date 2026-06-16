423ce634768f273478987f65a924f115b7243a44
Paolo Ursua <astheressix31416@gmail.com>
2026-06-16 17:14:08 -0500

    feat: add initial database schema and seed data for the product table

--- backend/appRelacionesjpa/src/main/java/com/negocio/repository/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.negocio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.negocio.entidades.Curso;
-
-public interface CursoRepository  extends JpaRepository<Curso, Integer>{
-
-}
--- backend/appRelacionesjpa/src/main/java/com/negocio/repository/MatriculaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.negocio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.negocio.entidades.Matricula;
-
-public interface MatriculaRepository extends JpaRepository<Matricula, Integer>{
-
-}
